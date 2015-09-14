0b52fa88e53b14ea6801637f84cfc876c980d622
Dane Stuckel <dane.stuckel@gmail.com>
2015-09-14 18:43:03 -0400

    remember previously seen articles

--- fetch/reuters.py
+++ fetch/reuters.py
@@ -10,10 +10,30 @@ from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
 pp = pprint.PrettyPrinter(indent=2)
+
+def load_as_json(filename, default):
+    try:
+        with open(filename, 'r') as f:
+            f = open(filename, 'r')
+            data = json.load(f)
+    except Exception as ex:
+        data = default
+    return data
+
+def save_as_json(data, filename):
+    f = open(filename, 'w')
+    json.dump(data, f, allow_nan=False, skipkeys=True, ensure_ascii=False, sort_keys=False, check_circular=False)
+    f.close()
+
+prev_dataset = load_as_json('.tmp/dataset', dict())
+prev_article_links = load_as_json('.tmp/article_links', set())
+prev_visited_articles = load_as_json('.tmp/visited_articles', set())
+
 links = set()
-visited_links = set()
-article_links = set()
-dataset = {}
+visited_links = set(prev_visited_articles)
+article_links = set(prev_article_links)
+visited_articles = set(prev_visited_articles)
+dataset = dict(prev_dataset)
 
 article_link_regex = r'reuters.com/article'
 start_url = "http://www.reuters.com"
@@ -40,6 +60,8 @@ def get_page(url):
     local_article_links = [x for x in local_links if re.search(article_link_regex, str(x)) != None]
 
     if is_article(url):
+        print('found new article!')
+        visited_articles.add(url)
         canonical = get_html_node(soup, 'link', {'rel':'canonical'}, 'href')
         fn_title = get_html_node(soup, 'meta', {'property':'og:title'}, 'content')
         keywords = get_html_node(soup, 'meta', {'name':'keywords'}, 'content')
@@ -63,7 +85,7 @@ def get_page(url):
 
 get_page(start_url)
 
-while len(links.difference(visited_links)) and len(visited_links) < 10:
+while len(links.difference(visited_links)) and len(visited_links) < 20 + len(prev_visited_articles):
     for link in set(links).difference(visited_links):
         get_page(link)
 
@@ -75,4 +97,10 @@ pp.pprint(visited_links) #needs to be db
 print('\nfound articles:\n')
 pp.pprint(article_links)
 print('\nfound dataset:\n')
-pp.pprint(dataset)
+pp.pprint(dataset )
+
+# save seen articles, so we don't fetch them again
+os.makedirs('.tmp', exist_ok=True)
+save_as_json(list(article_links), '.tmp/article_links')
+save_as_json(list(visited_articles), '.tmp/visited_articles')
+save_as_json(dataset, '.tmp/dataset')
